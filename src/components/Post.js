13b171e21cfcb8b09f105cc6df1e34dc7981ff5b
Courgette <tuyenphan@kms.com.vn>
2021-05-20 13:14:24 +0700

    Style Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,17 @@
 import showdown from 'showdown';
 
 export default function Post(props) {
-  const { post, category } = props;
+  const { post } = props;
   const mdConverter = new showdown.Converter();
 
   return (
     <section>
-      <p>{category}</p>
-      <h1>{post.title}</h1>
-      <p>{post.description}</p>
+      <h1 className="text-3xl font-bold text-center">{post.title}</h1>
+      <p className="text-sm italic text-center text-gray-600">
+        {post.description}
+      </p>
       <div
-        className="markdown"
+        className="grid grid-flow-row gap-4 my-7 markdown"
         dangerouslySetInnerHTML={{
           __html: mdConverter.makeHtml(post.content),
         }}
